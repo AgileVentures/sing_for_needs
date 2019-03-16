54c73a5acf67a6a07f3a5079138f6d4e2919ac0d
Federico Esparza <federicoesparza@outlook.com>
2019-03-16 11:37:21 -0500

    Update to elixir 1.8.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SingForNeeds.Mixfile do
     [
       app: :sing_for_needs,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.8.1",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
