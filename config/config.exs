6f927c9724ef652e8588d6c88a30d8f718c68331
Federico Esparza <federicoesparza@outlook.com>
2019-03-16 11:47:40 -0500

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -11,7 +11,10 @@ config :sing_for_needs, SingForNeedsWeb.Endpoint,
   secret_key_base: "RnmcWcNx67dA+IAkmQEmQAa5BzRW3LByzxM/48XD6u0uPH8NQKXcNB/VQiJ3QOws",
   render_errors: [view: SingForNeedsWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: SingForNeeds.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+           adapter: Phoenix.PubSub.PG2],
+  live_view: [
+            signing_salt: "r0kI0N07ukawYsEnwFgkTNSK64Qr80FT"
+          ]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -22,3 +25,6 @@ config :logger, :console,
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
 config :phoenix, :json_library, Jason
+
+config :phoenix, :template_engines, leex: Phoenix.LiveView.Engine
+
