e92c0abda433a6c8fc18a291aa9acbe6660fbab4
Federico Esparza <federicoesparza@outlook.com>
2019-04-22 14:07:17 -0500

    run mix phx.gen.json Artists Artist artists name:string AND add resource to /api scope

---
+++ lib/sing_for_needs/artists/artist.ex
@@ -0,0 +1,17 @@
+defmodule SingForNeeds.Artists.Artist do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "artists" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(artist, attrs) do
+    artist
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
