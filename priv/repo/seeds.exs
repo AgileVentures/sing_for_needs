e49d515cc1a062baecaa7d727b52257280209568
aonomike <aonomike@gmail.com>
2019-05-01 12:52:17 +0300

    Refactor to remove multiple lines

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,5 +11,4 @@
 # and so on) as they will fail if something goes wrong.
 alias SingForNeeds.Artists
 
-(1..100)
-  |>Enum.each(fn (number) -> Artists.create_artist(%{name: "Awesome Artist #{number}"}) end)
+Enum.each((1..100), fn (number) -> Artists.create_artist(%{name: "Awesome Artist #{number}"}) end)
