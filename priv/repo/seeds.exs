73dcf038d408c8ef7c68308311861cc9a7c3d9d2
aonomike <aonomike@gmail.com>
2019-05-01 12:43:22 +0300

    Add seed data for Artist resource

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias SingForNeeds.Artists
+
+(1..100)
+  |>Enum.each(fn (number) -> Artists.create_artist(%{name: "Awesome Artist #{number}"}) end)
