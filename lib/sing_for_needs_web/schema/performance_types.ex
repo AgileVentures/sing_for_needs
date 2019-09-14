3f23bcb4ffd186d8b97ad565fd90fd4226cb235f
aonomike <aonomike@gmail.com>
2019-09-14 21:44:48 +0300

    Add graphql schema for PerformanceType

---
+++ lib/sing_for_needs_web/schema/performance_types.ex
@@ -0,0 +1,11 @@
+defmodule SingForNeedsWeb.Schema.PerformanceTypes do
+    @moduledoc """
+    All types for Performance
+    """
+    use Absinthe.Schema.Notation
+    object :performance do
+      field :id, :id
+      field :detail, :string
+      field :amount_raised, :currency
+    end
+end
