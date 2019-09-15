8cc17f512373a7ade25e5e9dd4f8d7788256dc29
aonomike <aonomike@gmail.com>
2019-09-15 23:12:09 +0300

    Update formating

--- lib/sing_for_needs_web/router.ex
+++ lib/sing_for_needs_web/router.ex
@@ -13,17 +13,17 @@ defmodule SingForNeedsWeb.Router do
     plug CORSPlug, origin: ["http://localhost:3000", "http://127.0.0.1:3000"]
     plug :accepts, ["json"]
   end
-  
+
   scope "/" do
     pipe_through :api
 
-    forward "/api", Absinthe.Plug,
-      schema: SingForNeedsWeb.Schema.Schema
+    forward "/api", Absinthe.Plug, schema: SingForNeedsWeb.Schema.Schema
 
     forward "/graphiql", Absinthe.Plug.GraphiQL,
       schema: SingForNeedsWeb.Schema.Schema,
       socket: SingForNeedsWeb.UserSocket,
       interface: :simple
   end
+
   # Other scopes may use custom stacks.
 end
