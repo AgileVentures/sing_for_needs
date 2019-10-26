e2a3537a34ae4dbda4f846994bc268e71e4d8a34
Mike Aono <aonomike@gmail.com>
2019-10-26 18:46:03 +0300

    remove unused methods

--- lib/sing_for_needs_web/resolvers/artists.ex
+++ lib/sing_for_needs_web/resolvers/artists.ex
@@ -6,13 +6,4 @@ defmodule SingForNeedsWeb.Resolvers.Artist do
   def artists(_parent, _args, _resolution) do
     {:ok, Artists.list_artists()}
   end
-
-  def artists_for_cause(cause, criteria, %{context: %{loader: loader}}) do
-    loader
-    |> Dataloader.load_many(Artists, {:artists, criteria}, cause)
-    |> on_load(fn loader ->
-      artists = Dataloader.get_many(loader, Artists, {:artists, criteria}, cause)
-      {:ok, artists}
-    end)
-  end
 end
