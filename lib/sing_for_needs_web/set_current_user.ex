5b1a73d3f4f7d54607479b2a3d05f9bf24e1fa93
aonomike <aonomike@gmail.com>
2019-12-13 16:50:11 +0300

    Fix linting issues

--- lib/sing_for_needs_web/set_current_user.ex
+++ lib/sing_for_needs_web/set_current_user.ex
@@ -1,13 +1,16 @@
 # Adds an Absinthe execution context to the Phoenix connection.
-# If a valid auth token is in the request header, the corresponding 
-# user is added to the context which is then available to all 
+# If a valid auth token is in the request header, the corresponding
+# user is added to the context which is then available to all
 # resolvers. Otherwise, the context is empty.
 #
-# This plug runs prior to `Absinthe.Plug` in the `:api` router 
+# This plug runs prior to `Absinthe.Plug` in the `:api` router
 # so that the context is set up and `Absinthe.Plug` can extract
 # the context from the connection.
 
 defmodule SingForNeedsWeb.Plugs.SetCurrentUser do
+  @moduledoc """
+  Plug to set the current user
+  """
   @behaviour Plug
   import Plug.Conn
 
